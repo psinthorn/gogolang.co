e0228e9eff501caf318c210578b09c70718ff2cf
Sinthorn Pradutnam <psinthorn@gmail.com>
2020-05-21 17:47:04 +0700

    update Index COntrollers

--- app/urls_mapping.go
+++ app/urls_mapping.go
@@ -1,12 +1,13 @@
 package app
 
 import (
+	controllers "github.com/psinthorn/gogolang.co/controllers"
 	"github.com/psinthorn/gogolang.co/controllers/ping"
-	controllers "github.com/psinthorn/gogolang.co/controllers/users"
 )
 
 func urlsMapping() {
 	router.GET("/ping", ping.Pong)
 	router.POST("/users", controllers.Users.CreateUser)
 	router.GET("/users/:user_id", controllers.Users.GetUser)
+	router.GET("/", controllers.IndexPage.Welcome)
 }
